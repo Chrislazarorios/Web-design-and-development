e9401672dcc25a19b8b71fda41826b801d841d7c
ChrisLopezRios <chris-dmt@hotmail.com>
2019-03-14 23:20:31 -0300

    Add files via upload

---
+++ htdocs/curso-jQuery/ej-05-ComponentesTerceros/js/codigos.js
@@ -0,0 +1,31 @@
+/*
+Analisis e Implementación de Componentes.
+
+Análisis:
+	1)Ver el funcionamiento del componente.
+	2)Ver que archivos necesito (js, css, img, fonts, etc).
+	3)Analizar el código html y js que lo hace funcionar.
+	4)LEER y REVISAR la DOCUMENTACIÓN, NO SEAS FLOJO y quieras todo FÁCIL. Esto hará que puedas sacar el máximo provecho del componente. 
+
+Implementación:
+	1)Nunca te adaptes a la estructura de la carpeta demo, ADAPTA el componente a la carpeta de tu proyecto.
+	2)Copia los archivos necesarios a la estructura de tu carpetas (css, js, img, etc).
+	3)Si el componente usa elementos multimedia, busca sus url en el css del mismo y reemplázalas por la nueva ruta que tendrán.
+	4)En el css del componente nunca trates de cambiar atributos, puedes hacer que deje de funcionar, si acaso modifica colores para que se adapte al diseño de tu proyecto.
+
+	http://flexslider.woothemes.com
+	https://fancyapps.com/fancybox/3/
+
+*/
+
+function efectos()
+{
+	$('.flexslider').flexslider({
+		animation : 'slide',
+		// direction : 'vertical', // Se anima en sentido vertical 
+		// slideShow : false // No se anima automaticamente
+		//controlNav : false // Cambia el estilo de navegacion, pueden ser puntos, thumbnails, true para mostrar o false para ocultar
+	})
+}
+
+$(document).ready(efectos)
